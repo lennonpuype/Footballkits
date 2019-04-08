2b28eca658c35bf0911ac179b70eca4ccbbd5312
Lennon Puype <lennonpuype@hotmail.com>
2019-04-08 15:42:19 +0200

    Tijdelijke tekst word succesvol naar een sqlite gestuurd. Volgende stap is de ingevulde velden juist versturen

--- iPresent/Controller/AddKitViewController.swift
+++ iPresent/Controller/AddKitViewController.swift
@@ -7,20 +7,41 @@
 //
 
 import UIKit
+import CoreData
 
 class AddKitViewController: UIViewController {
 
 
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-       
     }
     
+    func saveKit(playername: String, team: String, moreinfo: String){
+       
+        
+       let kit = Kit(context:context);
+        kit.playername = playername;
+        kit.team = team;
+        kit.moreinfo = moreinfo;
+
+        do{
+            try context.save();
+        } catch {
+            print("Error saving context: \(error)");
+        }
+
+        print(FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first!)
+        
+    }
+    
+    @IBAction func saveKitButton(_ sender: Any) {
+        self.saveKit(playername: "Messi", team: "Barcelona", moreinfo: "I got this shirt on a trip in Barcelona");
+    }
 
     /*
     // MARK: - Navigation
